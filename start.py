d212864a5c094348a26bd08cac812210d6cd8063
emart <emart@emartui-MacBookPro-49.local>
2024-01-19 14:41:05 +0900

    update page decoration

--- start.py
+++ start.py
@@ -1,9 +1,23 @@
+# app.py
+
 import streamlit as st
 import pandas as pd
+import numpy as np
+from PIL import Image
+from time import sleep
 from datetime import datetime as dt
 import datetime
 
 
+
+
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon="🐶",
+    page_title="이마트 StoreProduct개발2팀 Streamlit",
+    layout="wide",
+)
 with st.sidebar:
     date = st.date_input(
         "조회 시작일을 선택해 주세요",
@@ -15,6 +29,36 @@ with st.sidebar:
         value='',
         placeholder='종목코드를 입력해 주세요'
     )
+
+
+
+# 로딩바 구현하기
+with st.spinner(text="페이지 로딩중..."):
+    sleep(2)
+
+# 페이지 헤더, 서브헤더 제목 설정
+st.header("이마트 StoreProduct개발2팀페이지에 오신걸 환영합니다👋")
+st.subheader("스트림릿 기능 맛보기")
+
+# 페이지 컬럼 분할(예: 부트스트랩 컬럼, 그리드)
+cols = st.columns((1, 1, 2))
+cols[0].metric("10/11", "15 °C", "2")
+cols[0].metric("10/12", "17 °C", "2 °F")
+cols[0].metric("10/13", "15 °C", "2")
+cols[1].metric("10/14", "17 °C", "2 °F")
+cols[1].metric("10/15", "14 °C", "-3 °F")
+cols[1].metric("10/16", "13 °C", "-1 °F")
+
+# 라인 그래프 데이터 생성(with. Pandas)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+# 컬럼 나머지 부분에 라인차트 생성
+cols[2].line_chart(chart_data)
+
+
+
 
 # 버튼 클릭
 button = st.button('버튼을 눌러보세요')
